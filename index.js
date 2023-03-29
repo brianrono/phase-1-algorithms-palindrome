f2761e1001b225233f10195133003495aca670f4
brianrono <brianrono16@gmail.com>
2023-03-29 11:24:29 +0300

    Completed

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 function isPalindrome(word) {
+  for (let i = 0; i < word.length / 2; i++) {
+    // check if the character at index i matches the character at the corresponding index counting backwards from the end
+    if (word[i] !== word[word.length - 1 - i]) {
+      return false; // not a palindrome if they don't match
+    }
+  }
+  return true; // if we get through the whole loop without finding a mismatch, it's a palindrome
   // Write your algorithm here
 }
 
@@ -7,7 +14,13 @@ function isPalindrome(word) {
 */
 
 /*
-  Add written explanation of your solution here
+We start by looping over the characters in the string, using for (let i = 0; i < word.length / 2; i++). 
+We only need to loop through the first half of the string, since we're comparing each character to 
+its corresponding character counting backwards from the end of the string.
+Inside the loop, we check whether the character at index i matches the character at the corresponding index 
+counting backwards from the end. We do this using if (word[i] !== word[word.length - 1 - i]). 
+If the characters don't match, we know it's not a palindrome, so we return false.
+If we get through the whole loop without finding a mismatch, we know it's a palindrome, so we return true.
 */
 
 // You can run `node index.js` to view these console logs
